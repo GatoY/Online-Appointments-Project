7dd04a781cdf99d26fb4cf8134c1c42c91c931fb
GatoY <derakhy@gmail.com>
2018-05-19 21:15:22 +1000

    url

--- OAP/OAP/settings.py
+++ OAP/OAP/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'oap',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Msqlmima',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
